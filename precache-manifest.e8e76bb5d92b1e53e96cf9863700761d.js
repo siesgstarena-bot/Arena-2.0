6e864bb8b6c7878de1c23f8232ef3b31833098ff
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-05-02 06:57:36 +0000

    Deploy siesgstarena/Arena-2.0 to github.com/siesgstarena/Arena-2.0.git:gh-pages

---
+++ precache-manifest.e8e76bb5d92b1e53e96cf9863700761d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d7e0534f880dd11aeb6",
+    "url": "/Arena-2.0/static/css/main.af87c5f5.chunk.css"
+  },
+  {
+    "revision": "1d7e0534f880dd11aeb6",
+    "url": "/Arena-2.0/static/js/main.1d7e0534.chunk.js"
+  },
+  {
+    "revision": "3ded5d546ce2550ba4b2",
+    "url": "/Arena-2.0/static/css/1.db984348.chunk.css"
+  },
+  {
+    "revision": "3ded5d546ce2550ba4b2",
+    "url": "/Arena-2.0/static/js/1.3ded5d54.chunk.js"
+  },
+  {
+    "revision": "8f81d8257f617a514fe3",
+    "url": "/Arena-2.0/static/js/runtime~main.8f81d825.js"
+  },
+  {
+    "revision": "4f4734e2ff821fe417a5e6018bee269c",
+    "url": "/Arena-2.0/static/media/tshirt.4f4734e2.jpg"
+  },
+  {
+    "revision": "ab2ee490f0133cfb10a1cd44058d4af3",
+    "url": "/Arena-2.0/static/media/bag.ab2ee490.jpg"
+  },
+  {
+    "revision": "a7e6457e48f6028f4dfd7857d50661ff",
+    "url": "/Arena-2.0/static/media/coders.a7e6457e.jpg"
+  },
+  {
+    "revision": "57e5cdd1f473dc82df474e251e5fac37",
+    "url": "/Arena-2.0/static/media/coders1.57e5cdd1.jpg"
+  },
+  {
+    "revision": "3127fb30bcb2e0eec686d08365869ad7",
+    "url": "/Arena-2.0/index.html"
+  }
+];
